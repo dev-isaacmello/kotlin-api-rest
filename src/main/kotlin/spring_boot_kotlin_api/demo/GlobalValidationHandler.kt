37af2abf9b6670ee8f6b3ffced0fc26a02d17c95
Isaac Mello <134700983+dev-isaacmello@users.noreply.github.com>
2026-01-30 15:45:04 -0300

    some changes and global validation

---
+++ src/main/kotlin/spring_boot_kotlin_api/demo/GlobalValidationHandler.kt
@@ -0,0 +1,21 @@
+package spring_boot_kotlin_api.demo
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalValidationHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationError(e: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
+        val errors = e.bindingResult.allErrors.map {
+            it.defaultMessage ?: "Invalid value"
+        }
+
+        return ResponseEntity
+            .status(400)
+            .body(mapOf("erros" to errors))
+    }
+}
