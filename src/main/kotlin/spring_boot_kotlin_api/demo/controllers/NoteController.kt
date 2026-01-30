44118e5878a9d151a79f138e4d23cf2c759dc32c
Isaac Mello <134700983+dev-isaacmello@users.noreply.github.com>
2026-01-30 11:50:55 -0300

    create a save method

--- src/main/kotlin/spring_boot_kotlin_api/demo/controllers/NoteController.kt
+++ src/main/kotlin/spring_boot_kotlin_api/demo/controllers/NoteController.kt
@@ -1,14 +1,24 @@
 package spring_boot_kotlin_api.demo.controllers
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import org.bson.types.ObjectId
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.*
+import spring_boot_kotlin_api.demo.database.model.Note
+import spring_boot_kotlin_api.demo.database.repository.NoteRepository
 import java.time.Instant
+
 
 @RestController
 @RequestMapping("/notes")
-class NoteController {
+class NoteController(
+    private val repository: NoteRepository,
+    private val noteRepository: NoteRepository
+) {
     data class NoteRequest(
         val id: String?,
+        @field:NotBlank(message = "Name is required!")
         val title: String,
         val content: String,
         val color: Long
@@ -22,7 +32,32 @@ class NoteController {
         val createdAt: Instant
     )
 
-    fun save(body: NoteRequest): NoteResponse {
+    @PostMapping
+    fun save(
+        @Valid @RequestBody body: NoteRequest
+    ): NoteResponse {
+        val ownerId = SecurityContextHolder.getContext().authentication!!.principal as String
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId(ownerId)
+            )
+        )
 
+        return note.toResponse()
     }
+
+
+
+    private fun Note.toResponse(): NoteResponse = NoteResponse(
+        id = id.toHexString(),
+        title = title,
+        content = content,
+        color = color,
+        createdAt = createdAt
+    )
 }
